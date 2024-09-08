32c3b3eb6aacd934b8fef630bbf07008867e8825
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-09-08 18:48:39 +0300

    Implemented TicTacToeMiniApp interface for interacting with external entities

--- Sources/TicTacToeMiniApp/TicTacToeMiniApp.swift
+++ Sources/TicTacToeMiniApp/TicTacToeMiniApp.swift
@@ -1,2 +1,29 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import UIKit
+import MiniAppCore
+
+public final class TicTacToeMiniApp: MiniAppProtocol {
+    
+    // MARK: - Public Properties
+    
+    public var title: String = Const.miniAppTitle
+    
+    public var icon: UIImage = UIImage(systemName: Const.miniAppIcon) ?? UIImage()
+    
+    public private(set) var view: UIView = TicTacToeView.create()
+    
+    // MARK: - Public Initializers
+    
+    public init() {}
+    
+    // MARK: - Public Methods
+    
+    public func configure(with configuration: MiniAppConfiguration) {
+        if let ticTacToeView = view as? TicTacToeView {
+            ticTacToeView.configure(
+                with: configuration.backgroundColor,
+                textColor: configuration.textColor,
+                textFont: configuration.font
+            )
+        }
+    }
+}
