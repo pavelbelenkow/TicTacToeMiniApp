9563a767236a3aee29c1232513a53ecd4136f98d
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-09-08 18:40:22 +0300

    Added MiniAppCore dependency to package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "TicTacToeMiniApp",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TicTacToeMiniApp",
             targets: ["TicTacToeMiniApp"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pavelbelenkow/MiniAppCore", from: "1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TicTacToeMiniApp"),
+            name: "TicTacToeMiniApp",
+            dependencies: ["MiniAppCore"]
+        ),
         .testTarget(
             name: "TicTacToeMiniAppTests",
             dependencies: ["TicTacToeMiniApp"]),
